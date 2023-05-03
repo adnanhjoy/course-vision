5fdf02d45f7188a10a8e378a8aa308e8c71dace9
engadnanhossain <engr.adnanhossain@gmail.com>
2023-05-03 09:54:09 +0600

    added signup auth

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 
 const Register = () => {
+    const { createUser } = useContext(AuthContext);
+
+
+    const handleSubmitRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const fname = form.fname.value;
+        const lname = form.lname.value;
+        const name = fname + " " + lname;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+        .then(result => {
+            result.user;
+            form.reset()
+        })
+        .catch(error => console.error(error))
+    }
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -11,41 +32,41 @@ const Register = () => {
                         <p className="py-6"></p>
                     </div>
                     <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
-                        <div className="card-body w-full">
+                        <form onSubmit={handleSubmitRegister} className="card-body w-full">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">First Name</span>
                                 </label>
-                                <input type="text" placeholder="first name" className="input input-bordered" />
+                                <input type="text" placeholder="first name" className="input input-bordered" name='fname' />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Last Name</span>
                                 </label>
-                                <input type="text" placeholder="last name" className="input input-bordered" />
+                                <input type="text" placeholder="last name" className="input input-bordered" name='lname' />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="email" placeholder="email" className="input input-bordered" name='email' />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="password" placeholder="password" className="input input-bordered" name='password'/>
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn btn-primary">Signup</button>
                                 <label className="label">
-                                    <small>Already have an account? <Link to='/login' className='link link-hover'>Login</Link></small>
+                                    <small>Already have an account? <Link to='/login' className='link link-hover'>login</Link></small>
                                 </label>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState({displayName: 'Adnan Hossain'})
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = 
+    {
+        user,
+        createUser
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
